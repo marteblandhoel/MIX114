ba279b6454f3806ddb2ba99c0146a38588c8b1dd
marteblandhoel <marte.blandhoel@gmail.com>
2024-05-10 15:00:53 +0200

    legge til google map API på carmode side

--- dinreise/carmode.js
+++ dinreise/carmode.js
@@ -67,3 +67,57 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+function initMap() {
+  const mapOptions = {
+    center: { lat: 60.3898141, lng: 5.3333388 },
+    zoom: 13,
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: true,
+    zoomControl: true,
+  };
+
+  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  const directionsService = new google.maps.DirectionsService();
+
+  const request = {
+    origin: "Bergen Sentrum, Bergen",
+    destination: "Nesttun Terminal, Bergen",
+    travelMode: google.maps.TravelMode.DRIVING,
+    provideRouteAlternatives: true,
+  };
+
+  directionsService.route(request, (response, status) => {
+    if (status === "OK") {
+      response.routes.forEach((route, index) => {
+        const directionsRenderer = new google.maps.DirectionsRenderer({
+          map: map,
+          suppressMarkers: false,
+          preserveViewport: true,
+          routeIndex: index,
+        });
+        directionsRenderer.setDirections(response);
+
+        // Assume the first leg of this route
+        const midPointIndex = Math.floor(route.legs[0].steps.length / 2);
+        const midPoint = route.legs[0].steps[midPointIndex].start_location;
+
+        const contentString = `<div style="padding: 5px; background: white; border: 1px solid black;">Route ${
+          index + 1
+        }: Time: ${route.legs[0].duration.text}, Distance: ${
+          route.legs[0].distance.text
+        }</div>`;
+
+        const infoBox = new google.maps.InfoWindow({
+          content: contentString,
+          position: midPoint,
+        });
+
+        infoBox.open(map);
+      });
+    } else {
+      window.alert("Directions request failed due to " + status);
+    }
+  });
+}
